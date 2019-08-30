d423ecd437a2c57357120a4afe404a170ff50b5f
saifoelloh <saifoelloh@gmail.com>
2019-08-31 00:00:26 +0700

    creating backend of supplier

--- src/app/Http/Controllers/SupplierController.php
+++ src/app/Http/Controllers/SupplierController.php
@@ -7,79 +7,95 @@ use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('pages.supplier.index');
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $suppliers = Supplier::all();
+    return view('pages.supplier.index', ['suppliers' => $suppliers]);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('pages.supplier.create');
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('pages.supplier.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    Supplier::create([
+      'name' => $request-.name,
+      'phone' => $request-.phone,
+      'address' => $request-.address,
+    ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Supplier  $supplier
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    return redirect('supplier');
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Supplier  $supplier
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        return view('pages.supplier.edit');
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Supplier  $supplier
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Supplier  $supplier
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Supplier  $supplier
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    $supplier = Supplier::with('item')->where('id', $id)->get();
+    return view('pages.supplier.edit', ['supplier' => $supplier]);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Supplier  $supplier
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Supplier  $supplier
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $supplier = Supplier::find($id);
+    $supplier->update([
+      'name' => $request-.name,
+      'phone' => $request-.phone,
+      'address' => $request-.address,
+    ]);
+
+    return redirect('supplier');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Supplier  $supplier
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    Supplier::where('id', $id)->delete();
+    return redirect('supplier');
+  }
 }
