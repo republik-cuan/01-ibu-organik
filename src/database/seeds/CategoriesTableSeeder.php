b17fa2bc562b361c29ca0b2da265d68310b478e1
haqqer <ilhamserbaguna@gmail.com>
2019-08-31 18:10:34 +0700

    add Seeder for Categories and Suppliers

---
+++ src/database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'name' => Str::random(10),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),            
+        ]);
+    }
+}
---
+++ src/database/seeds/SuppliersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class SuppliersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('suppliers')->insert([
+            'name' => Str::random(10),
+            'phone' => '085842388761',
+            'address' => Str::random(10),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    }
+}
