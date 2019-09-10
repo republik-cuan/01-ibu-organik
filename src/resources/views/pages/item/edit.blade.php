06190fa7961e3e1a1d880efa8a64a741ca6981da
saifoelloh <saifoelloh@gmail.com>
2019-09-11 00:49:21 +0700

    refactoring views

--- src/resources/views/pages/item/edit.blade.php
+++ src/resources/views/pages/item/edit.blade.php
@@ -20,42 +20,46 @@
 @section('content')
 <div class="box box-danger">
   <div class="box-body">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
+    <div class="row">
+      <div class="col-md-6">
         <form action="{{route ('item.update', $item->id)}}" method="post">
-            {{ csrf_field() }}
-            {{ method_field('put') }}
-            <div class="form-group">
-              <label for="name">Nama</label>
+          @csrf
+          @method('put')
+          <div class="form-group">
+            <label for="name">Nama</label>
             <input type="text" class="form-control" name="name" value="{{ $item->name }}" placeholder="Nama Item" autofocus>
-              {!! $errors->first('name', '<p class="help-block text-danger">:message</p>')!!}
-              <label for="price">Harga</label>
-              <input type="number" class="form-control" name="price" value="{{ $item->price }}" placeholder="Harga Item" autofocus>
-              {!! $errors->first('price', '<p class="help-block text-danger">:message</p>')!!}
-              <label for="number">Stok</label>
-              <input type="text" class="form-control" name="stock" value="{{ $item->stock }}" placeholder="Stok Item" autofocus>
-              {!! $errors->first('stock', '<p class="help-block text-danger">:message</p>')!!}
-              <label for="categoy_id">Kategori</label>
-              <select name="category_id" id="" class="form-control">
-                <option disabled selected>{{ $item->category->name}}</option>
-                @foreach ($categories as $category)
-                  <option value="{{ $category->id }}">{{ $category->name }}</option>
-                @endforeach
+            {!! $errors->first('name', '<p class="help-block text-danger">:message</p>')!!}
+            <label for="modal">Modal</label>
+            <input type="number" class="form-control" name="modal" value="{{ $item->modal }}" placeholder="Modal" autofocus>
+            {!! $errors->first('price', '<p class="help-block text-danger">:message</p>')!!}
+            <label for="reseller">Reseller</label>
+            <input type="number" class="form-control" name="reseller" value="{{ $item->reseller }}" placeholder="Reseller" autofocus>
+            {!! $errors->first('price', '<p class="help-block text-danger">:message</p>')!!}
+            <label for="endUser">End User</label>
+            <input type="number" class="form-control" name="endUser" value="{{ $item->endUser }}" placeholder="End User" autofocus>
+            {!! $errors->first('price', '<p class="help-block text-danger">:message</p>')!!}
+            <label for="number">Stok</label>
+            <input type="text" class="form-control" name="stock" value="{{ $item->stock }}" placeholder="Stok Item" autofocus>
+            {!! $errors->first('stock', '<p class="help-block text-danger">:message</p>')!!}
+            <label for="categoy_id">Kategori</label>
+            <select name="category_id" id="" class="form-control">
+              <option disabled selected>{{ $item->category->name}}</option>
+              @foreach ($categories as $category)
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+              @endforeach
             </select>
-              {!! $errors->first('categoy_id', '<p calss="help-block text-danger">:message</p>')!!}
-              <label for="supplier_id">Supplier</label>
-              <select name="supplier_id" id="" class="form-control">
-                  <option disabled selected>{{ $item->supplier->name }}</option>
-                  @foreach ($suppliers as $supplier)
-                    <option value="{{ $supplier->id }}">{{ $supplier->name }}</option>
-                  @endforeach
-              </select>
-              {!! $errors->first('supplier_id', '<p calss="help-block text-danger">:message</p>')!!}
-            </div>
-            <button type="submit" class="btn btn-info">submit</button>
-          </form>
-        </div>
+            {!! $errors->first('categoy_id', '<p calss="help-block text-danger">:message</p>')!!}
+            <label for="supplier_id">Supplier</label>
+            <select name="supplier_id" id="" class="form-control">
+              <option disabled selected>{{ $item->supplier->name }}</option>
+              @foreach ($suppliers as $supplier)
+                <option value="{{ $supplier->id }}">{{ $supplier->name }}</option>
+              @endforeach
+            </select>
+            {!! $errors->first('supplier_id', '<p calss="help-block text-danger">:message</p>')!!}
+          </div>
+          <button type="submit" class="btn btn-info">submit</button>
+        </form>
       </div>
     </div>
   </div>
