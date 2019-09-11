a51ff930d0b56997b57374e8be3d99bbaad87c37
wdwiramadhan <wahyu_dr71@yahoo.com>
2019-09-11 22:30:06 +0700

    fix categories datatabled

--- src/resources/views/pages/category/index.blade.php
+++ src/resources/views/pages/category/index.blade.php
@@ -53,26 +53,23 @@
 
 @section('js')
 <script>
-  $(document).ready(function() {
-    $('.data-table').dataTable({
-      data: {!! $categories !!},
-      columns: [{
-          data: 'id'
-        },
-        {
-          data: 'name'
-        },
-        {
-          data: 'id',
-          render: function(data) {
-            const link = "{{route('category')}}"+"/"+data;
-            const detail = '<a class="btn btn-primary btn-xs" stlye="margin: 0 3px" href="' + link + ' ">edit</a>';
-            const hapus = '<form role="form" action="' + link + '" style="margin: 0 3px;display:inline" method="POST">{{ csrf_field()}}{{method_field('delete ')}}<button class="btn btn-danger btn-xs">delete</button></form>';
-            return '<div class="text-center">' + detail + hapus + '</div>';
-          }
-        },
-      ]
-    });
-  });
-</script>
+        $(document).ready(function() {
+            $('.data-table').dataTable({
+                data: {!! $categories !!},
+                columns: [
+                    {data: 'id'},
+                    {data: 'name'},
+                    {
+                        data: 'id',
+                        render: function(data){
+                            const link ="{{route('category')}}"+"/"+data;
+                            const detail = '<a class="btn btn-primary btn-xs" stlye="margin: 0 3px" href="'+link+'">edit</a>';
+                            const hapus = '<form role="form" action="'+link+'" stlye="margin: 0 3px;display:inline" method="POST">{{ csrf_field()}}{{method_field('delete')}}<button class="btn btn-danger btn-xs">delete</button></form>';
+                            return '<div class="text-center">'+detail+hapus+'</div>';
+                        }
+                    }
+                ]
+            });
+        });
+    </script>
 @stop
