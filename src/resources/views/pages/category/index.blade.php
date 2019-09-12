b872cc3e899681ebcc4de5f654760317b6178bc8
saifoelloh <saifoelloh@gmail.com>
2019-09-12 19:43:06 +0700

    refactoring views code

--- src/resources/views/pages/category/index.blade.php
+++ src/resources/views/pages/category/index.blade.php
@@ -34,12 +34,12 @@
   <div class="box-body">
     <div class="row">
       <div class="col-md-12">
-        <table class="data-table table table-bordered table-hover">
+        <table class="data-table table table-bordered table-hover text-center">
           <thead>
             <tr>
-              <th class="text-center">No</th>
-              <th class="text-center">Nama</th>
-              <th class="text-center">Aksi</th>
+              <th>No</th>
+              <th>Nama</th>
+              <th>Aksi</th>
             </tr>
           </thead>
         </table>
@@ -73,7 +73,7 @@
             const link = "{{route('category')}}"+"/"+data;
             const detail = '<a class="btn btn-primary btn-xs" stlye="margin: 0 3px" href="' + link + ' ">edit</a>';
             const hapus = '<form role="form" action="' + link + '" style="margin: 0 3px;display:inline" method="POST">{{ csrf_field()}}{{method_field('delete ')}}<button class="btn btn-danger btn-xs">delete</button></form>';
-            return '<div class="text-center">' + detail + hapus + '</div>';
+            return '<div>' + detail + hapus + '</div>';
           }
         },
       ]
