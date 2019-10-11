526576baab4b3d17c7f1db3f33cdb5e66aa3a985
wdwiramadhan <wahyu_dr71@yahoo.co.id>
2019-10-11 16:10:26 +0700

    adding migration for bank

---
+++ src/database/migrations/2019_10_09_063638_create_banks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('bank');
+            $table->string('rekening');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banks');
+    }
+}
