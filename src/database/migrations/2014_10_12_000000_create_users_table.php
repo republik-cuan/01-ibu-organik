8f5edb89e942205af05191b2d940a66af22a5f04
saifoelloh <saifoelloh@gmail.com>
2019-10-12 08:07:18 +0700

    define default value of role

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->enum('role', ['super admin', 'admin']);
+            $table->enum('role', ['super admin', 'admin'])->default('admin');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
