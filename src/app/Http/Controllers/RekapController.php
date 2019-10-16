115aa1a583a264fb493013757dbf8247e9faa2a2
saifoelloh <saifoelloh@gmail.com>
2019-10-16 23:17:18 +0700

    update rekap by item

--- src/app/Http/Controllers/RekapController.php
+++ src/app/Http/Controllers/RekapController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Item;
 use App\Purchase;
-use App\Exports\PurchaseExport;
+use App\Exports\ItemExport;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
 
 class RekapController extends Controller
@@ -17,86 +16,38 @@ class RekapController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function item()
     {
       $items = Item::with('purchases:purchase_id,statusHarga')->get();
-      $purchases = Purchase::with(['inventories.item:id,modal,reseller,endUser'])->get()->groupBy(function($item){
-        return Carbon::parse($item->created_at)->format('Y-m');
-      });
+
+      return view('pages.rekap.item', [
+        'items' => $items,
+      ]);
+    }
+
+    public function itemExport() {
+      return Excel::download(new ItemExport, 'rekap-barang.xlsx');
+    }
+
+    public function month(Request $request) {
+      $purchases = [];
+      if ($request!=null) {
+        $purchases = Purchase::with(['inventories.item:id,modal,reseller,endUser'])
+          ->whereYear('created_at', $request->year)
+          ->whereMonth('created_at', $request->month)
+          ->get();
+      } else {
+        $purchases = Purchase::with(['inventories.item:id,modal,reseller,endUser'])->get()->groupBy(function($item){
+          return Carbon::parse($item->created_at)->format('Y-m');
+        });
+      }
 
       return view('pages.rekap.index', [
-        'items' => $items,
         'purchases' => $purchases,
       ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
-    public function itemExport() {
-      return Excel::download(new PurchaseExport, 'rekap.xlsx');
+    public function monthExport() {
+      return Excel::download(new PurchaseExport, 'rekap-bulanan.xlsx');
     }
 }
---
+++ src/resources/views/pages/rekap/item.blade.php
@@ -0,0 +1,108 @@
+@extends('adminlte::page')
+
+@section('title', 'Rekap Barang')
+
+@section('content_header')
+    <h1>Rekap Barang</h1>
+@stop
+
+@section('content')
+  <div class="box box-danger">
+    <div class="box-body">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-6 text-right">
+          <a class="btn btn-success" href="{{route('rekap.export-item')}}" style="margin-bottom: 10px;">Cetak</a>
+        </div>
+        <div class="col-md-12">
+          <table id="table-item" class="data-table table table-bordered table-hover text-center">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Terjual</th>
+                <th>Modal</th>
+                <th>Total Harga</th>
+                <th>Margin</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+@stop
+
+
+@section('js')
+<script charset="utf-8">
+  let subtotal = 0;
+  let margin = 0;
+  $(document).ready(function(){
+    $('#table-item').dataTable({
+      data: {!! $items !!},
+      columns: [{
+          data: 'id',
+          render: function(data) {
+            return arguments[3].row+=1;
+          },
+        },
+        {
+          data: 'name'
+        },
+        {
+          data: 'sold',
+        },
+        {
+          data: 'modal',
+          render: function(data) {
+            return `Rp. ${new Intl.NumberFormat().format(data)}`;
+          }
+        },
+        {
+          data: 'purchases',
+          render: function(data) {
+            let harga = 0;
+            let hasil = 0;
+            subtotal = 0;
+            margin = 0;
+            if (data.length > 0) {
+             data.map((datum) => {
+                switch(datum.statusHarga) {
+                  case 'reseller':
+                    harga = arguments[2].reseller;
+                    break;
+                  case 'end user':
+                    harga = arguments[2].endUser;
+                    break;
+                  case 'modal':
+                    harga = arguments[2].modal;
+                    break;
+                }
+                subtotal += (harga * datum.pivot.total);
+              });
+            } else {
+              subtotal = 0;
+            }
+            return `Rp. ${new Intl.NumberFormat().format(subtotal)}`;
+          },
+        },
+        {
+          data: 'purchases',
+          render: function(data) {
+            let harga = 0;
+            let hasil = 0;
+            if (data.length>1) {
+              data.map((datum) => {
+                margin += (arguments[2].modal * datum.pivot.total);
+              });
+            } else {
+              margin = 0;
+            }
+            return `Rp. ${new Intl.NumberFormat().format(subtotal-margin)}`;
+          },
+        },
+      ]
+    });
+  });
+</script>
+@endsection
