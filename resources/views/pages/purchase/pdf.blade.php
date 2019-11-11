7eb1fb2797bca7275a568c46c74327ba3c6c9d44
saifoelloh <saifoelloh@gmai.com>
2019-11-11 21:33:28 +0700

    update invoice UI

--- resources/views/pages/purchase/pdf.blade.php
+++ resources/views/pages/purchase/pdf.blade.php
@@ -8,30 +8,34 @@
   <title>Invoice | {{$purchase->kode}}</title>
 </head>
 <body>
-  <table>
+  <table class="w-100">
     <tr>
       <th>No Invoice</th>
-      <td class="px-3">{{$purchase->kode}}</td>
+      <td>{{$purchase->kode}}</td>
+      <td class="text-right">{{ date_format($purchase->created_at, "l, d-m-Y") }}</td>
     </tr>
     <tr>
       <th>Nama</th>
-      <td class="px-3">{{$customer->name}}</td>
-    </tr>
-    <tr>
-      <th>Alamat</th>
-      <td class="px-3">{{$customer->address}}</td>
+      <td>{{$customer->name}}</td>
+      <td class="text-right">{{ ucwords($purchase->deliveryOption) }}</td>
     </tr>
     <tr>
       <th>Telp</th>
-      <td class="px-3">{{$customer->phone}}</td>
+      <td>{{$customer->phone}}</td>
     </tr>
     <tr>
-      <th>Delivery By</th>
-      <td class="px-3">{{$purchase->deliveryOption}}</td>
+      <th>Rekening</th>
+      <td>
+        {{ $purchase->bank->bank." | ".$purchase->bank->rekening }}
+      </td>
     </tr>
     <tr>
-      <th>Date</th>
-      <td class="px-3">{{date_format($purchase->created_at, "d M Y")}}</td>
+      <th>Alamat</th>
+      <td>{{$customer->address}}</td>
+    </tr>
+    <tr>
+      <th>Patokan</th>
+      <td>{{ucwords($customer->patokan)}}</td>
     </tr>
   </table>
   <table class="w-100 my-5 text-center" border="1">
@@ -39,7 +43,6 @@
       <tr>
         <th>No</th>
         <th>Nama</th>
-        <th>Diskon</th>
         <th>Jumlah(gram)</th>
         <th>Harga</th>
         <th>Sub Harga</th>
@@ -56,13 +59,7 @@
       @foreach ($inventories as $key => $item)
       <tr>
         <td>{{$key+=1}}</td>
-        <td>{{$item->item->name}}</td>
-        <td>
-          @php
-            $discount += $item->discount;
-            echo "Rp. ".number_format($item->discount, 2);
-          @endphp
-        </td>
+        <td>{{ ucwords($item->item->name) }}</td>
         <td>
           @php
             $berat += $item->total;
@@ -96,20 +93,37 @@
       </tr>
     @endforeach
       <tr>
-        <th colspan="2">Total</th>
-        <td></td>
+        <th colspan="5" class="text-left px-2">
+          Diskon
+        </th>
+      </tr>
+      @foreach ($inventories as $key => $item)
+        <tr>
+          <td>{{ $key+=1 }}</td>
+          <td>{{ ucwords($item->item->name) }}</td>
+          <td>{{ $item->total." gram" }}</td>
+          <td colspan="2" class="text-left px-4">{{ "Rp. ".number_format($item->discount, 2) }}</td>
+        </tr>
+      @endforeach
+      <tr>
+        <td colspan="5">
+          <span class="text-white">hello</span>
+        </td>
+      </tr>
+      <tr>
+        <th colspan="2" class="text-left px-2">Total</th>
         <td>{{number_format($berat, 2)}}</td>
         <td>{{"Rp. ".number_format($harga,2)}}</td>
         <td>{{"Rp. ".number_format($subTotal,2)}}</td>
       </tr>
       <tr>
-        <th colspan="2">Delivery</th>
-        <td colspan="3"></td>
+        <th colspan="2" class="text-left px-2">Delivery</th>
+        <td colspan="2"></td>
         <td>{{"Rp. ".number_format($purchase->deliveryPrice,2)}}</td>
       </tr>
       <tr>
-        <th colspan="2">Gran Total</th>
-        <td colspan="3"></td>
+        <th colspan="2" class="text-left px-2">Grand Total</th>
+        <td colspan="2"></td>
         <td>{{"Rp. ".number_format($purchase->deliveryPrice+$subTotal,2)}}</td>
       </tr>
     </tbody>
